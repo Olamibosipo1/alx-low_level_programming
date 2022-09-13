a69a6aeccb60bbb701bb33898d146718a1b736f7
Olabosipo <olamibosipo1@gmail.com>
2022-09-13 21:31:47 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <main.h>
+#include <stdio.h>
+/**
+ * main - Print Alphabet
+ *
+ * Return: 0
+ **/
+
+char main(void)
+{	char 'a'
+
+		while
+		('a' <= 'z');
+	{
+		putchar (a);
+	}
+		a++;
+
+	{
+		putchar("\n");
+	}
+
+
+
+
+}
+return (0);
