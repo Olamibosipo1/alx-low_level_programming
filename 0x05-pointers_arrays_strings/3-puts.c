1f7392b4ab21ad60fb7aa89e206259230de40510
olabosipo <olamibosipo1@gmail.com>
2022-09-22 09:19:30 +0000

    Puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _puts - function that prints a string
+* @str: string printout
+*
+* Return: string
+*/
+
+void _puts(char *str)
+{
+	_putchar(str);
+	return (str);
+}
