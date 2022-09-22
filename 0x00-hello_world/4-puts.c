5ac735913b975e9b5b5684999c3798ae1aae74ff
olabosipo <olamibosipo1@gmail.com>
2022-09-22 19:28:57 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,14 @@
-#!/bin/bash
-gcc puts ("Programming is like building a multilingual puzzle")
-0
+#include<stdio.h>
+
+/**
+ * main - Program to print
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
