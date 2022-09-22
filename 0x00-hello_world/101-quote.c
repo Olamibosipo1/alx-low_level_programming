f98d9f13f75bad5851ade70569efb936e17a6da0
olabosipo <olamibosipo1@gmail.com>
2022-09-22 23:00:31 +0000

    Quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+/**
+* main - Print text to standard error
+* @void: Print text
+*
+* Return: 1
+*/
+
+int main(void)
+
+{
+	char i[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, i, sizeof(i));
+	return (1);
+
+}
+
