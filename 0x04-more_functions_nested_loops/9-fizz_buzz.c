439b585d29900ccc7d722c40a160998a5e89eb12
olabosipo <olamibosipo1@gmail.com>
2022-12-17 12:43:45 +0000

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,35 +11,37 @@
  *
  * Return: always 0
  */
-
 int main(void)
 
 {
-	int a = 100;
-	int b;
 
-	b = 1;
-	while (b <= a)
+	int a = 1;
+
+	while (a <= 100)
 	{
-		if (b % 3 == 0 && b % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (b % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-
-		else if (b % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else
-		{
-			printf("%d ", b);
-		}
-		b++;
-		printf("\n");
-		return (0);
+	if (a % 3 == 0 && a % 5 == 0)
+	{
+	printf("FizzBuzz");
 	}
+	else if (a % 3 == 0)
+	{
+	printf("Fizz");
+	}
+	else if (a % 5 == 0)
+	{
+	printf("Buzz");
+	}
+	else
+	{
+	printf("%d", a);
+	}
+	if (a < 100)
+	{
+		printf(" ");
+	}
+	a++;
+	}
+
+	printf("\n");
+	return (0);
 }
