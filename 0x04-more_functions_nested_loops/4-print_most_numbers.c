65083f6d8c605046a0fd076420e5b6cb7cc2c0bf
olabosipo <olamibosipo1@gmail.com>
2022-12-17 09:22:58 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
  * print_most_numbers - function that prints the numbers,
  * from 0 to 9, followed by a new line.
  *
- * Return : (void)
+ * Return: void
  */
 void print_most_numbers(void)
 {
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints 10 times the numbers from
+ * 0-14, followed by a new line
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+
+{
+	int a, b;
+
+	for (a = 0; a <= 14; a++)
+	for (b = 0; b = 10; b++)
+	{
+		_putchar('a');
+		_putchar(b % 10 + '0');
+	}
+	{
+		_putchar('\n');
+	}
+}
