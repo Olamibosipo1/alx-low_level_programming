ca9e08965c830339e5fe2fae16d496a8600dcfa6
olabosipo <olamibosipo1@gmail.com>
2022-12-17 09:58:10 +0000

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,16 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a <= 14; a++)
+	for (a = 1; a <= 10; a++)
 	{
-	for (b = 0; b = 10; b++)
+	for (b = 0; b < 15; b++)
 	{
-		if (a >= 10)
-		_putchar('a');
-		_putchar(a % 10 + '0');
+	if (b >= 10)
+
+		_putchar('1');
+		_putchar(b % 10 + '0');
 	}
-	{
-		_putchar('\n');
-	}
-	}
+
+	_putchar('\n');
+}
 }
