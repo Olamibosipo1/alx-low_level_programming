b70681000b803359d1cfc78a9d993c23f74599c8
olabosipo <olamibosipo1@gmail.com>
2022-12-21 22:11:19 +0000

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strcmp -function that compares two strings
+ * @s1: String
+ * @s2: String
+ *
+ * Return: 0 if equal, -ve if s1 < s2, +ve if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+		int ret = strcmp(s1, s2);
+
+			return (ret);
+}
