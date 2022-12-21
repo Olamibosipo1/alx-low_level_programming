d85c591a39da1706344f70fe87fda19b6fa73c80
olabosipo <olamibosipo1@gmail.com>
2022-12-21 22:47:18 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include <string.h>
+
+/**
+ * cap_string - Functions that capitalizes all words of a string
+ * @s: String to be capitalized
+ *
+ * Return: Capitalized string
+ */
+
+
+char *cap_string(char *s)
+
+{
+		int i = 0, j;
+
+			char separator[] = {' ', '\t', '\n', ',', ';', '.',
+					'!', '?', '"', '(', ')', '{', '}'};
+
+			if (s[0] >= 'a' && s[0] <= 'z')
+		{
+			s[0] -= 32;
+		}
+			while (s[i] != '\0')
+		{
+			j = 0;
+
+			while (j < 13)
+			{
+
+			if (s[i] == separator[j])
+			}
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			s[i + 1] -= 32;
+		}
+		}
+			j++;
+		}
+
+		i++;
+	}
+	return (s);
+}
