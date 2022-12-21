82eb9bd920b96a314f4b28a3b4932bfd408fea16
olabosipo <olamibosipo1@gmail.com>
2022-12-21 21:10:56 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,33 +5,13 @@
  * @dest: variable
  *
  * @src: variable
- * Return: void
+ * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 
 {
 
-		int i;
-		int j;
-
-			i = 0;
-				while (dest[i] != '\0')
-		{
-				i++;
-		}
-			j = 0;
-
-			while (src[j] != '\0')
-
-		{
-			dest[i] = src[j];
-
-			i++;
-
-			j++;
-		}
-
-			dest[i] = '\0';
-			return (dest);
+	dest = strcat(dest, src);
+	return (dest);
 }
