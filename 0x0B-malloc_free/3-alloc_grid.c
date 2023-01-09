d289c3089bea691d0a0485cd0902c8be0aae0fee
Olabosipo <olamibosipo1@gmail.com>
2023-01-09 22:04:23 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,50 +1,33 @@
 #include <stdlib.h>
-
+#include <stdio.h>
+#include "main.h"
 /**
- * *alloc_grid - Allocate memory to a 2-dimensional array of integers.
- * @width: Width of array
- * @height: Height of array
- *
- * Return: Pointer to array on success, NULL on failure.
+ * *alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: columns
+ * @height: rows
+ * Return: pointer to 2d array
  */
-
 int **alloc_grid(int width, int height)
-
 {
-	int **twoDimArray;
-	int i = 0, j;
-
+	int **grid;
+	int i, j;
 	if (width <= 0 || height <= 0)
-	return (NULL);
-	twoDimArray = malloc(sizeof(int *) * height);
-	if (twoDimArray == NULL)
-	return (NULL);
-
-	while (i < height)
-	{
-		twoDimArray[i] = malloc(sizeof(int) * width);
-		if (twoDimArray[i] == NULL)
-		{
-			free(twoDimArray);
-			j = 0;
-			while (j <= i)
-		{
-			free(twoDimArray[j]);
-			j++;
-		}
 		return (NULL);
-	}
-	i++;
-	{	i = 0;
-	while (i < height)
+	grid = malloc(height * sizeof(int *));
+	if (grid == NULL)
+	return (NULL);
+	for (i = 0; i < height; i++) 
 	{
-		j = 0;
-		while (j < width)
+	grid[i] = malloc(width * sizeof(int));
+	if (grid[i] == NULL)
 		{
-			twoDimArray[i][j] = 0;
-			j++;
+		for (i = 0; i < height; i++)
+		free(grid[i]);
+		free(grid);
+		return (NULL);
 		}
-		i++;
-	}
-	return (twoDimArray);
+		for (j = 0; j < width; j++)
+		grid[i][j] = 0;
+		}
+	return (grid);
 }
