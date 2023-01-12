6aee1a10d115061d5bf9d1e6f2d9af458fca41f8
Olabosipo <olamibosipo1@gmail.com>
2023-01-12 20:41:46 +0000

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *malloc_checked - funtion that allocates memory using malloc
+ *
+ * @b: number of bytes to be allocated from the heap.
+ * Return: Pointer to the allocated space address.
+ */
+
+void *malloc_checked(unsigned int b)
+
+{
+
+	void *ptr = malloc(b);
+
+		if (ptr == NULL)
+
+		{
+		exit(98);
+		}
+
+		return (ptr);
+}
