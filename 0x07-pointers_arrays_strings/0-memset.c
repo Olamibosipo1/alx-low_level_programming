258c98950158c8d9e1bc41a0df9f4a6d3b035b2c
Olabosipo <olamibosipo1@gmail.com>
2023-01-22 20:41:07 +0000

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	char *_string = memset(s, b, n);
+	unsigned int i;
 
-	return (_string);
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
 }
+
