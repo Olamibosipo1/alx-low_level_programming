34fb4ac5c5967743015bc794ae8aa8fa6f26d367
Olabosipo <olamibosipo1@gmail.com>
2023-02-01 21:15:47 +0000

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,6 +2,7 @@
 
 /**
  * print_listint - a function that prints all elements of a list
+ *
  * @h: pointer to first node
  *
  * Return: the number of nodes
@@ -16,9 +17,12 @@ size_t print_listint(const listint_t *h)
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
+
 		h = h->next;
+
 		node_count++;
 	}
-		printf("%d\n", h->n;
+		printf("%d\n", h->n);
+
 		return (node_count);
 }
